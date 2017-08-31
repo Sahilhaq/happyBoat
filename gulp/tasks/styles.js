cd2a55b194a853f8846541337865ed96700d3e8d
Sahil <sahilhaque24.10@gmail.com>
2017-08-31 16:21:37 +0530

    Happy boat Completed homepage desgin

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,15 +3,18 @@ postcss = require('gulp-postcss'),
 cssImport = require('postcss-import'),
 nested = require('postcss-nested'),
 cssvars = require('postcss-simple-vars'),
+mixins = require('postcss-mixins'),
+autoprefixer = require('autoprefixer'),
 hexrgba = require('postcss-hexrgba');
 
 
-gulp.task('styles', function(){
+gulp.task('styles', function(callback){
    return gulp.src('./app/assets/styles/style.css')
-            .pipe(postcss([cssImport, hexrgba, nested, cssvars]))
+            .pipe(postcss([cssImport, mixins, autoprefixer, cssvars, hexrgba, nested]))
             .on('error', function(errorInfo){
                 console.log(errorInfo.toString());
                 this.emit('end');
+					callback();
             })
             .pipe(gulp.dest('./app/temp/styles'));
 });
